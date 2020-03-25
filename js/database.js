12d7f8478247349a125afba130f1d85fd46690a1
Roshan Chokshi <chokshiroshan@gmail.com>
2020-03-26 00:11:16 +0530

    Resolved data button bug.

--- js/database.js
+++ js/database.js
@@ -137,47 +137,45 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $.getJSON(
-    "https://cryptic-ravine-96718.herokuapp.com/",
-    null,
-    function(data) {
-      var news = document.getElementById("news");
-      var newcol = document.createElement("ul");
-      newcol.setAttribute("class", "list-inline");
-      news.appendChild(newcol);
-      for (var i = 0; i < 12; i++) {
-        var li = document.createElement("li");
-        li.setAttribute("class", "list-inline-item");
-        var card = document.createElement("div");
-        card.setAttribute("class", "card");
-        card.style.width = "15rem";
-        card.style.marginRight = "1rem";
-        card.style.marginBottom = "2rem";
-        // card.style.border="2px solid black";
-        card.style.boxShadow = "none";
-        var card_title = document.createElement("h5");
-        card_title.innerHTML = data.news[i].title;
-        card_title.setAttribute("classs", "card-title");
-        var news_img = document.createElement("img");
-        news_img.setAttribute("src", data.news[i].img);
-        news_img.setAttribute("class", "card-img-top");
-        var btntoart = document.createElement("a");
-        btntoart.setAttribute("class", "btn btn-main");
-        btntoart.style.color = "#fff";
-        btntoart.style.background = "#000";
-        btntoart.setAttribute("href", data.news[i].link);
-        btntoart.innerHTML = "Read More";
-        var card_body = document.createElement("div");
-        card_body.setAttribute("class", "card-body");
-        card_body.appendChild(card_title);
-        card_body.appendChild(btntoart);
-        card.appendChild(news_img);
-        card.appendChild(card_body);
-        li.appendChild(card);
-        newcol.appendChild(li);
-      }
+  $.getJSON("https://cryptic-ravine-96718.herokuapp.com/", null, function(
+    data
+  ) {
+    var news = document.getElementById("news");
+    var newcol = document.createElement("ul");
+    newcol.setAttribute("class", "list-inline");
+    news.appendChild(newcol);
+    for (var i = 0; i < 12; i++) {
+      var li = document.createElement("li");
+      li.setAttribute("class", "list-inline-item");
+      var card = document.createElement("div");
+      card.setAttribute("class", "card");
+      card.style.width = "15rem";
+      card.style.marginRight = "1rem";
+      card.style.marginBottom = "2rem";
+      // card.style.border="2px solid black";
+      card.style.boxShadow = "none";
+      var card_title = document.createElement("h5");
+      card_title.innerHTML = data.news[i].title;
+      card_title.setAttribute("classs", "card-title");
+      var news_img = document.createElement("img");
+      news_img.setAttribute("src", data.news[i].img);
+      news_img.setAttribute("class", "card-img-top");
+      var btntoart = document.createElement("a");
+      btntoart.setAttribute("class", "btn btn-main");
+      btntoart.style.color = "#fff";
+      btntoart.style.background = "#000";
+      btntoart.setAttribute("href", data.news[i].link);
+      btntoart.innerHTML = "Read More";
+      var card_body = document.createElement("div");
+      card_body.setAttribute("class", "card-body");
+      card_body.appendChild(card_title);
+      card_body.appendChild(btntoart);
+      card.appendChild(news_img);
+      card.appendChild(card_body);
+      li.appendChild(card);
+      newcol.appendChild(li);
     }
-  );
+  });
 });
 
 let resp = null;
@@ -214,6 +212,22 @@ $(document).ready(function() {
     var map = document.getElementById("map-data");
     graph.style.display = "none";
     map.style.display = "block";
+  });
+});
+
+$(document).ready(function() {
+  $("#btn-offical").on("click", function() {
+    var offical = document.getElementById("offical-data");
+    var fast = document.getElementById("fast-data");
+    offical.style.display = "block";
+    fast.style.display = "none";
+  });
+
+  $("#btn-fast").on("click", function() {
+    var offical = document.getElementById("offical-data");
+    var fast = document.getElementById("fast-data");
+    offical.style.display = "none";
+    fast.style.display = "block";
   });
 });
 
